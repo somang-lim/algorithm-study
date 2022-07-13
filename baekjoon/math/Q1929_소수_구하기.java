7b69ac1dd97c6b7f1e4dc6334b7f656c160254fd
somang-lim <somangjuha@gmail.com>
2022-07-13 22:22:16 +0900

    [백준] 1929_소수_구하기

---
+++ baekjoon/math/Q1929_소수_구하기.java
@@ -0,0 +1,35 @@
+package baekjoon.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1929_소수_구하기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        for(int i = M; i <= N; i++) {
+            if(isPrime(i)) {
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static boolean isPrime(int num) {
+        if(num == 1) return false;
+
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+
+}
