54279463dbb1e584445d238d6e458e385af16eca
somang-lim <somangjuha@gmail.com>
2022-07-13 22:26:29 +0900

    [백준] 1978_소수 찾기

--- baekjoon/math/Q1978_소수_찾기.java
+++ baekjoon/math/Q1978_소수_찾기.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 실버 5
+// ⏰ 84ms
 public class Q1978_소수_찾기 {
 
     public static void main(String[] args) throws IOException {
