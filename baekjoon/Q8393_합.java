c59734d2ef9559ef0cbac9bdfda420a17b6ccd18
somang-lim <somangjuha@gmail.com>
2022-07-13 11:10:43 +0900

    [백준] 8393_합

---
+++ baekjoon/Q8393_합.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 브론즈 5
+// ⏰ 80ms
+public class Q8393_합 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * (n + 1) / 2);
+    }
+
+}
